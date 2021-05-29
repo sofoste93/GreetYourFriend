8212793fe99c9a4958bdd5dc51cb950b22ad9004
Stephane Sob Fouodji <stefan-kruger7@hotmail.com>
2021-05-29 13:07:09 +0200

    Add project files.

---
+++ GreetFriends/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreetFriends
+{
+	class Program
+	{
+		static string outMsg = "Hey {0}, am glad to see you here!";
+		static string endMsg = "Press any key to close this window..";
+		static void Main(string[] args)
+		{
+			Console.Write("Enter your friend's name: ");
+			string friendName = Console.ReadLine();
+
+			GreetYourFriend(friendName);
+			Console.WriteLine("\n" + endMsg);
+			Console.ReadKey();
+		}
+
+		public static void GreetYourFriend(string yourFriendName)
+		{
+			Console.WriteLine(outMsg, yourFriendName);
+		}
+	}
+}
